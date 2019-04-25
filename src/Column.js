c5db2ba0b52748c41e0365740ae69f88d7844384
Pavel Smolin <isingwithaz@gmail.com>
2019-04-25 20:44:11 +0300

    Lesson 11 horizontal alignment

--- src/Column.js
+++ src/Column.js
@@ -8,7 +8,6 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
-  width: 220px;
   display: flex;
   flex-direction: column;
 `;
@@ -18,14 +17,13 @@ const Title = styled.h3`
 const TaskList = styled.div`
   padding: 8px;
   background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
-  flex-grow: 1;
-  min-height: 100px;
+  display: flex;
 `;
 
 const Column = ({ column: { id, title } = {}, isDropDisabled, tasks }) => (
   <Container>
     <Title>{title}</Title>
-    <Droppable droppableId={id} type={id === 'column-3' ? 'done' : 'active'} isDropDisabled={isDropDisabled}>
+    <Droppable direction="horizontal" droppableId={id} type={id === 'column-3' ? 'done' : 'active'} isDropDisabled={isDropDisabled}>
       {(provided, snapshot) => (
         <TaskList
           ref={provided.innerRef}
--- src/Task.js
+++ src/Task.js
@@ -3,35 +3,37 @@ import styled from 'styled-components';
 import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
-  border: 1px solid lightgrey;
+  border: 3px solid lightgrey;
   padding: 8px;
   margin-bottom: 8px;
-  border-radius: 2px;
-  background-color: ${props =>
-    props.isDragDisabled ? 'lightgrey' : props.isDragging ? 'lightgreen' : 'white'};
+  border-radius: 50%;
   display: flex;
-`;
+  width: 40px;
+  height: 40px;
+  margin: 8px;
 
-const Handle = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: orange;
-  border-radius: 4px;
-  margin-right: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:focus {
+    outline: none;
+    border-color: orange;
+  }
 `;
 
 const Task = ({ index, task: { content, id } = {} }) => {
   const isDragDisabled = id === 'task-1';
   return (
-    <Draggable draggableId={id} index={index} isDragDisabled={isDragDisabled}>
+    <Draggable draggableId={id} index={index}>
       {(provided, snapshot) => (
         <Container
           {...provided.draggableProps}
+          {...provided.dragHandleProps}
           ref={provided.innerRef}
           isDragging={snapshot.isDragging}
           isDragDisabled={isDragDisabled}>
-          <Handle {...provided.dragHandleProps} />
-          {content}
+          {content[0]}
         </Container>
       )}
     </Draggable>
