7fa5452825544095150e5d5a02f8b2a34f2269e1
Pavel Smolin <isingwithaz@gmail.com>
2019-04-25 20:06:14 +0300

    Lesson 9

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import '@atlaskit/css-reset';
 import { DragDropContext } from 'react-beautiful-dnd';
 
 import Column from './Column';
 import initialData from './initialData';
 
+const Container = styled.div`
+  display: flex;
+`;
+
 const App = () => {
   const [data, setData] = useState(initialData);
 
-  return data.columnOrder.map(columnId => {
-    const column = data.columns[columnId];
-    const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
+  const onDragEnd = ({ destination, draggableId, source }) => {
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
 
-    const onDragEnd = ({ destination, draggableId, source }) => {
-      if (!destination) return;
-      if (
-        destination.droppableId === source.droppableId &&
-        destination.index === source.index
-      )
-        return;
+    const start = data.columns[source.droppableId];
+    const finish = data.columns[destination.droppableId];
 
-      const column = data.columns[source.droppableId];
-      const newTaskIds = [...column.taskIds];
+    if (start === finish) {
+      const newTaskIds = [...start.taskIds];
       newTaskIds.splice(source.index, 1);
       newTaskIds.splice(destination.index, 0, draggableId);
 
       const newColumn = {
-        ...column,
+        ...start,
         taskIds: newTaskIds
       };
 
@@ -36,30 +40,57 @@ const App = () => {
       };
 
       setData(newData);
-    };
+    } else {
+      const startTaskIds = [...start.taskIds];
+      startTaskIds.splice(source.index, 1);
 
-    const onDragStart = () => {
-      document.body.style.color = 'orange';
-      document.body.style.transition = 'background-color 0.2s ease';
-    };
+      const newStart = { ...start, taskIds: startTaskIds };
 
-    const onDragUpdate = update => {
-      const { destination } = update;
-      const opacity = destination
-        ? destination.index / Object.keys(data.tasks).length
-        : 0;
-        document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
-    };
+      const finishTaskIds = [...finish.taskIds];
+      finishTaskIds.splice(destination.index, 0, draggableId);
 
-    return (
-      <DragDropContext
-        onDragEnd={onDragEnd}
-        onDragStart={onDragStart}
-        onDragUpdate={onDragUpdate}>
-        <Column key={column.id} column={column} tasks={tasks} />
-      </DragDropContext>
-    );
-  });
+      const newFinish = { ...finish, taskIds: finishTaskIds };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish
+        }
+      };
+
+      setData(newData);
+    }
+  };
+
+  const onDragStart = () => {
+    document.body.style.color = 'orange';
+    document.body.style.transition = 'background-color 0.2s ease';
+  };
+
+  const onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(data.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  };
+
+  return (
+    <DragDropContext
+      onDragEnd={onDragEnd}
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}>
+      <Container>
+        {data.columnOrder.map(columnId => {
+          const column = data.columns[columnId];
+          const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </Container>
+    </DragDropContext>
+  );
 };
 
 export default App;
